97e0e19228dd434926c0abf0ef7d4aa7febcda02
Alex Hamadey <128324646+ahamadey27@users.noreply.github.com>
2025-10-10 16:38:49 -0400

    Add RichestCustomerWealth class to calculate the maximum wealth of customers from their bank accounts

---
+++ data-structures-algorithms/Matrix/RichestCustomerWealth .cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace data_structures_algorithms.Matrix
+{
+    /*You are given an m x n matrix accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. public class RichestCustomerWealth
+
+    Return the wealth that the richest customer has.
+
+    Imagine every customer has multiple bank accounts, with each account holding a certain amount of money. The total wealth of a customer is calculated by summing all the money across all their multiple. */
+    public class RichestCustomerWealth
+    {
+        public int maximumWealth(int[][] accounts)
+        {
+            //Initialize maxWealth to 0
+            int maxWealth = 0;
+
+            //Loop through each customer's accounts 
+            foreach (int[] customer in accounts)
+            {
+                //Variable to store the wealth of the current customer
+                int wealth = 0;
+
+                //Loop through each account of the wealth customer to sum them up
+                foreach (int account in customer)
+                {
+                    wealth += account;
+                }
+
+                //Update maxWealth if the current customer's wealth is greater 
+                if (wealth > maxWealth)
+                {
+                    maxWealth = wealth;
+                }
+            }
+            return maxWealth;
+        }
+
+        
+    }
+}
--- data-structures-algorithms/obj/Debug/net9.0/data-structures-algorithms.AssemblyInfo.cs
+++ data-structures-algorithms/obj/Debug/net9.0/data-structures-algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("data-structures-algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e28ac36f9a4e388bf741ea2f078b0c9460ce23d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ab0d53310d2e7fe16ae0081ae14efdf2b50e87")]
 [assembly: System.Reflection.AssemblyProductAttribute("data-structures-algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("data-structures-algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
